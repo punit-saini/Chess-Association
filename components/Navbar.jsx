4c28c9f0abf72e5568730b3425d6493cd29b0754
chhattisgarhchess <developer.cgsca@gmail.comm>
2023-06-02 17:49:48 +0530

    hehe

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,9 +30,21 @@ export default ()=>{
 <nav className="bg-white text-my-black dark:bg-gray-900 w-full z-20 rounded-t-3xl border-b  dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="/" className="flex items-center">
-      <img src="../logo.png" className="h-12 w-16 mr-1" alt="CGSCA Logo" />
-      <span className="self-center text-xl font-semibold  whitespace-nowrap dark:text-white">Chhattisgarh Chess</span>
-  </a>
+           
+            <img
+              src="../logo.png"
+              className="h-12 w-16 ml-1"
+              alt="CGSCA Logo"
+            />
+             <div className="flex flex-col md:flex-row md:gap-1.5 gap-y-0 p-0">
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                Chhattisgarh State
+              </span>
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                Chess Association
+              </span>
+            </div>
+          </a>
   <div className="flex md:order-2">
       <Link href={'/registration'} type="button" className=" hidden md:block text-white bg-gradient-to-tr drop-shadow-lg from-my-green to-my-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 hover:bg-black text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Player Registration</Link>
       <button data-collapse-toggle="navbar-sticky" onClick={handleNavClick} type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
