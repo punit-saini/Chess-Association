97e82dd1a2bd526aca33cc5c239569c01ab5f8fa
chhattisgarhchess <developer.cgsca@gmail.comm>
2023-06-02 17:06:02 +0530

    again did it

--- pages/api/handlePayment.js
+++ pages/api/handlePayment.js
@@ -1,34 +1,34 @@
-import { useState } from "react";
-import { client } from "../../lib/client";
-import Instamojo from 'instamojo-payment-nodejs'
+// import { useState } from "react";
+// import { client } from "../../lib/client";
+// import Instamojo from 'instamojo-payment-nodejs'
 
 
 
-export default async function register(req,res){
-    console.log('inside the handlepayment page')
+// export default async function register(req,res){
+//     console.log('inside the handlepayment page')
 
-    Instamojo.isSandboxMode(true)
-    Instamojo.setKeys('test_b41c7403bd156541494caca0426', 'test_27607b580c5d182ea83da335c50')
+//     Instamojo.isSandboxMode(true)
+//     Instamojo.setKeys('test_b41c7403bd156541494caca0426', 'test_27607b580c5d182ea83da335c50')
     
 
-      const options = {
-        purpose: "Product name", // REQUIRED
-        amount: 150, // REQUIRED and must be > ₹3 (3 INR)
-        currency: "INR",
-        buyer_name: "Madan Kumar",
-        email: "punitwranz@gmail.com",
-        phone: null,
-        send_email: true,
-        send_sms: false,
-        allow_repeated_payments: false,
-        webhook: "",
-        redirect_url: "http://localhost:3000/api/webhook",
-      };
+//       const options = {
+//         purpose: "Product name", // REQUIRED
+//         amount: 150, // REQUIRED and must be > ₹3 (3 INR)
+//         currency: "INR",
+//         buyer_name: "Madan Kumar",
+//         email: "punitwranz@gmail.com",
+//         phone: null,
+//         send_email: true,
+//         send_sms: false,
+//         allow_repeated_payments: false,
+//         webhook: "",
+//         redirect_url: "http://localhost:3000/api/webhook",
+//       };
       
-      const paymentData = Instamojo.PaymentData(options);
-      console.log('before the payment call')
-      const response = await Instamojo.createNewPaymentRequest(paymentData);
-      console.log('response is : ', response)
+//       const paymentData = Instamojo.PaymentData(options);
+//       console.log('before the payment call')
+//       const response = await Instamojo.createNewPaymentRequest(paymentData);
+//       console.log('response is : ', response)
     
 
-}
+// }
